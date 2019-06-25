f812a7823b55aa0da2c5becddf08b8981757dcb6
heguangyu5 <heguangyu5@qq.com>
2019-06-25 18:19:34 +0800

    sync

--- lmdb-wrapper-vapi/lmdb-wrapper.h
+++ lmdb-wrapper-vapi/lmdb-wrapper.h
@@ -9,6 +9,7 @@ typedef struct LMDB_Db LMDB_Db;
 
 LMDB_Db *lmdb_db_new(const char *path, size_t db_size_in_mb);
 void lmdb_db_free(LMDB_Db *db);
+void lmdb_db_sync(LMDB_Db *db);
 uint8_t *lmdb_db_get(LMDB_Db *db, uint8_t *key, size_t key_len, size_t *data_len);
 bool lmdb_db_put(LMDB_Db *db, uint8_t *key, size_t key_len, uint8_t *data, size_t data_len);
 
